89a369b871aa36e529fae45601b6416ba612181a
Thierno Oumar DIALLO <thierno2.diallo@etu.univ-nantes.fr>
2019-07-04 23:11:24 +0200

    Setup and static pages

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+        <script src="https://kit.fontawesome.com/3c3db5688d.js"></script>
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap-theme.min.css" integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous">
+        <!-- Google font -->
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+        <title>Laravel</title>
+ 
+    </head>
+    <style>
+        body {
+            font-family: 'open sans', Helvetica, Arial, sans-serif;
+        }
+
+        footer {
+            margin: 4em 0;
+        }
+
+    </style>
+    <body>
+       
+        @include('layouts.partials._nav')
+
+        @yield('content')
+        
+        @include('layouts.partials._footer')
+
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    </body>
+</html>
---
+++ resources/views/layouts/partials/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="text-center">
+    <p>
+         &copy; {{date('Y')}} &middot; Laracaste by <a href="https://twitter.com/etsmo">@etsno</a> 
+         &dash; A <a href="">Laramap</a> clone app.
+    </p>
+    <p>
+        <b>This app has been built for learning purposes.</b>
+    </p>
+</footer>
